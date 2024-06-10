5780bcdafb291c22e5d4adc788fedb2931e4380f
bollemi <etu45187@henallux.be>
2024-06-10 13:45:25 +0200

    add node label

--- database-visualization-app/src/components/Visualization.tsx
+++ database-visualization-app/src/components/Visualization.tsx
@@ -19,7 +19,12 @@ export default function Visualization(props : VisualizationProps) {
         }
 
         const tmpNodes: Node[] = props.nodes.length === 0 ? [] :
-            props.nodes.map(n => ({id : n.id}));
+            props.nodes.map(n => {
+                const html = document.createElement('div');
+                html.innerHTML = n.name;
+                html.style.transform = 'translate(10%, 50%)';
+                return {id : n.id, html: html, size: 40}
+            });
         const tmpRels: Relationship[] = props.relations.length === 0 ? [] :
             props.relations.map(r => ({id: `${r.start}>>>${r.end}`, from: r.start, to: r.end}));
         setNodes(tmpNodes);
@@ -34,6 +39,6 @@ export default function Visualization(props : VisualizationProps) {
         nodes={nodes}
         rels={rels}
         style={{display: 'block'}}
-        nvlOptions={{minimapContainer: document.getElementById('content') || document.body}}
+        nvlOptions={{initialZoom: 0.75}}
     />;
 };
