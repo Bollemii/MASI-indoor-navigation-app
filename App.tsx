740cc157a495f3882b9b37448ba2bc41f28efaf2
bollemi <etu45187@henallux.be>
2024-07-25 15:31:29 +0200

    check environment variables setting

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { RootSiblingParent } from 'react-native-root-siblings';
+import { StyleSheet, Text, View } from 'react-native';
 
 import Router from '@/router/Router';
 import ContextsProvider from '@/context';
+import { colors } from '@/styles/colors';
 
 export default function App() {
+    const requiredEnvVars = [
+        process.env.EXPO_PUBLIC_NEO4J_URI,
+        process.env.EXPO_PUBLIC_NEO4J_USER,
+        process.env.EXPO_PUBLIC_NEO4J_PASSWORD,
+    ];
+    if (requiredEnvVars.includes(undefined)) {
+        console.error("Environment variables not set. Check .env.example file for more information.");
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>Environment variables not set.</Text>
+                <Text style={styles.text}>Check .env.example file for more information.</Text>
+            </View>
+        );
+    }
+
     return (
         <RootSiblingParent>
             <ContextsProvider>
@@ -14,3 +31,18 @@ export default function App() {
         </RootSiblingParent>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.white,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    text: {
+        marginVertical: 10,
+        fontSize: 16,
+        color: colors.red,
+    }
+});
